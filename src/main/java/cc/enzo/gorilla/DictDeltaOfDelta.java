a4144e2835afe2b6f6d015876964c6879ad9248c
Liu Enze <liuenze6516@gmail.com>
2020-09-12 10:50:46 +0800

    add todo

--- src/main/java/cc/enzo/gorilla/DictDeltaOfDelta.java
+++ src/main/java/cc/enzo/gorilla/DictDeltaOfDelta.java
@@ -6,6 +6,7 @@ public class DictDeltaOfDelta extends DeltaOfDelta {
     @Override
     protected void encode(ByteArrayDataOutput output, int deltaOfDelta) {
         //TODO: using dict to encode
+        //TODO: dict, freq, huffman encoding ??
         super.encode(output, deltaOfDelta);
     }
 
