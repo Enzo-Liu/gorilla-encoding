37cd0c0f5d730d07e6321c059909e2aa895da064
Liu Enze <liuenze6516@gmail.com>
2020-09-12 10:51:03 +0800

    fix typo

--- src/main/java/cc/enzo/gorilla/DictDeltaOfDelta.java
+++ src/main/java/cc/enzo/gorilla/DictDeltaOfDelta.java
@@ -12,7 +12,7 @@ public class DictDeltaOfDelta extends DeltaOfDelta {
 
     @Override
     public byte[] getBytes() {
-        //TODO: add dick encoding
+        //TODO: add dict encoding
         return super.getBytes();
     }
 }
